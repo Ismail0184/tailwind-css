34042536f44653a9865156bc420358f79d0deb77
Ismail0184 <ismailhossain600@gmail.com>
2022-10-09 12:04:07 +0600

    extend colors and fond family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        primary: '#fc1f15',
+        secondary: {
+          100: '#8d98ff',
+          200: '#636bff',
+        }
+      },
+      fontFamily: {
+        'body': ['Montserrat']
+      }
+
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
+
+
